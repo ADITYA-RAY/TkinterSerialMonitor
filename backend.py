7c29680954f472bff79bbb7c22014cd8448333f7
ADITYA RAY <aditya90005@gmail.com>
2023-08-20 11:30:30 +0530

    plotting sucess

--- backend.py
+++ backend.py
@@ -1,21 +1,50 @@
 import serial 
-import time 
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import threading
+import numpy
+
+
 
 class backend():
-    def __init__(self):
-        pass
-    
-    def connect(self,port, baudrate):
-        pass
+    def __init__(self,port,baudrate):
+        self.mcu = serial.Serial(port=port,baudrate=baudrate,timeout=0.1)
+        self.data = ""
+        self.x_axis = 0
+        plt.ion()
+        self.fig = plt.figure()
+        self.numx = [[0]*2]*2
+        self.numy = [[0]*2]*2
+        self.colors=["r-","b-"]
+  
+    def connect(self):
+        self.mcu.write(bytes("1",'utf-8'))
+        time.sleep(0.05)
 
     def getData(self):
-        psss
-
+        self.data = self.mcu.readline()
+        if self.data != bytes("",'utf-8'):
+            self.x_axis += 1 
+            decoded = ((self.data).decode()).split()
+            return decoded
+        return 
+            
     def plot(self):
-        pass
-
+        while True:
+            graphs = self.getData()
+            if graphs:
+                for i in range(len(graphs)):
+                    self.numx[i] = [self.numx[i][1],self.x_axis]
+                    self.numy[i] = [self.numy[i][1],int(graphs[i])]
+                    plt.plot(self.numx[i],self.numy[i],self.colors[i])
+                    self.fig.canvas.draw()     
+                    self.fig.canvas.flush_events()             
 
 if __name__ == "__main__":
-    process = backend()
-    backend.connect()
+    process = backend('/dev/ttyACM0','115200')
+    process.connect()
+    # process.getData()
+    process.plot()
+    # thread.start()
 
