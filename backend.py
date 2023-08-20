d3913f49e3f75a9078c934b2fc4a959c7acf3c6e
ADITYA RAY <aditya90005@gmail.com>
2023-08-20 09:58:39 +0530

    backend file initialized

---
+++ backend.py
@@ -0,0 +1,21 @@
+import serial 
+import time 
+
+class backend():
+    def __init__(self):
+        pass
+    
+    def connect(self,port, baudrate):
+        pass
+
+    def getData(self):
+        psss
+
+    def plot(self):
+        pass
+
+
+if __name__ == "__main__":
+    process = backend()
+    backend.connect()
+
